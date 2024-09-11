cc58b7e77d00be86ff3bcfda8adaa89553b9eb4a
Aleksandar Rokvic <rokvicaleksandar7@gmail.com>
2024-09-11 13:15:26 +0200

    Implement WebElements and signUp method for registration form automation.

--- src/test/java/Base/BaseTest.java
+++ src/test/java/Base/BaseTest.java
@@ -3,18 +3,23 @@ package Base;
 import Pages.RegistrationPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
 
 public class BaseTest {
 
     public static WebDriver driver;
+    public static WebDriverWait wait;
     public RegistrationPage registrationPage;
 
 
     @BeforeClass
     public void setUp() {
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(3));
         registrationPage = new RegistrationPage();
     }
 
@@ -22,4 +27,8 @@ public class BaseTest {
     public void tearDown() {
        // driver.quit();
     }
+
+    //----------------------------------------------------------------
+
+    public String url = "https://etherscan.io/register";
 }
--- src/test/java/Pages/RegistrationPage.java
+++ src/test/java/Pages/RegistrationPage.java
@@ -1,6 +1,55 @@
 package Pages;
 
 import Base.BaseTest;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class RegistrationPage extends BaseTest {
+
+    public RegistrationPage() {
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(id = "ContentPlaceHolder1_txtUserName")
+    public WebElement usernameField;
+
+    @FindBy(id = "ContentPlaceHolder1_txtEmail")
+    public WebElement emailAddressField;
+
+    @FindBy(id = "ContentPlaceHolder1_txtConfirmEmail")
+    public WebElement confirmEmailAddressField;
+
+    @FindBy(id = "ContentPlaceHolder1_txtPassword")
+    public WebElement passwordField;
+
+    @FindBy(id = "ContentPlaceHolder1_txtPassword2")
+    public WebElement confirmPasswordField;
+
+    @FindBy(id = "ContentPlaceHolder1_MyCheckBox")
+    public WebElement termsAndConditionsCheckBox;
+
+    @FindBy(id = "ContentPlaceHolder1_SubscribeNewsletter")
+    public WebElement newsletterCheckBox;
+
+    @FindBy(id = "ContentPlaceHolder1_btnRegister")
+    public WebElement registerButton;
+
+    //-------------------------------------------------------
+
+    public void signUp(String username, String email, String confirmEmail, String password, String confirmPassword) {
+        usernameField.clear();
+        usernameField.sendKeys(username);
+        emailAddressField.clear();
+        emailAddressField.sendKeys(email);
+        confirmEmailAddressField.clear();
+        confirmEmailAddressField.sendKeys(confirmEmail);
+        passwordField.clear();
+        passwordField.sendKeys(password);
+        confirmPasswordField.clear();
+        confirmPasswordField.sendKeys(confirmPassword);
+        termsAndConditionsCheckBox.click();
+        newsletterCheckBox.click();
+        registerButton.click();
+    }
 }
